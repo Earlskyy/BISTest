069895e822b7a7b97eade980ed2e0146d723c34a
Earlskyy <earlstevenjpacaldo@gmail.com>
2026-02-26 21:47:15 +0800

    blue color

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -125,7 +125,7 @@ export default function Home() {
         className="bg-gray-100 py-16"
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h2 className="text-3xl font-bold text-red-900 mb-10 text-center">
+          <h2 className="text-3xl font-bold text-blue-900 mb-10 text-center">
            Latest Announcements 
           </h2>
           <AnnouncementsList />
